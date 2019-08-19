2a6ea368850e23c3d3c5c9e1377ec8e46a4df12d
Charles Danner <danner.ced1@gmail.com>
2019-08-18 22:21:56 -0400

    <api key added. have buttons loading onto screen with for loop going through topics and they are given classes and data attributes based on their names.>

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,25 @@
+$(document).ready(function () {
+
+var apiKey = "xpcO9JeJ5xXfr8vOQiZXgQKOetu5FX4C"
 var topics = ["Arya", "Sansa", "Game of Thrones", "Jon Snow", "Ned", "Cersei", 
                 "Jaime", "Joffrey", "The Hound", "The Mountain", "Hodor", "Daenarys", "Dragons"];
 
+function populateButtons(){
+    $("#buttons").empty();
+
+    for(var i = 0; i < topics.length; i++){
+        
+        var newGifButton = $("<button>").text(topics[i]);
+        newGifButton.addClass("gif-btn");
+        newGifButton.attr("data-name")
+        $("#buttons").append(newGifButton);
+    }
+}
+
+
+
+
+populateButtons();
+
+
+})
